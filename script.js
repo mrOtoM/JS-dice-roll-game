0515c2231635fa1bedc15832c60e91dfd53b46d0
mrOtoM <mrotomachala@gmail.com>
2022-10-19 12:17:47 +0200

    uprava pridanie script.js + prva logika

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.querySelector('#score--1');
+const diceElement = document.querySelector('.dice');
+
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
