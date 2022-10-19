8f897d7551a203f89e92a47a9afbdd5d4daf7f4e
mrOtoM <mrotomachala@gmail.com>
2022-10-19 13:11:29 +0200

    uprava switch player 2

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
-const diceElement = document.querySelector('.dice');
-const btnRoll = document.querySelector('.btn--roll');
-
 const current0Element = document.querySelector('#current--0');
 const current1Element = document.querySelector('#current--1');
+
+const diceElement = document.querySelector('.dice');
+const btnRoll = document.querySelector('.btn--roll');
 
 // Starting conditions
 score0Element.textContent = 0;
@@ -27,10 +29,12 @@ btnRoll.addEventListener('click', function () {
     currentScore += rollDice;
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-    current0Element.textContent = currentScore;
   } else {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
+    // currentScore = 0;
     currentScore = 0;
     activePlayer = activePlayer === 0 ? 1 : 0; //Change player
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
